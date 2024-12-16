c55006133d7fa95cd36e86fdef4dc161cbaffa52
IshG18 <161998412+IshG18@users.noreply.github.com>
2024-12-16 16:07:19 -0500

    Fix vector operations: __round__, __neg__, and correct div/sub behavior

--- myvect.py
+++ myvect.py
@@ -8,7 +8,6 @@ class Vector:
     def __add__(self, var):
         output = []
         if isinstance(var, Vector):
-            output = []
             length = self.len if self.len >= var.len else var.len
             for i in range(length):
                 value1 = self.nums[i] if i < self.len else 0
@@ -16,7 +15,7 @@ class Vector:
                 output.append(value1+value2)
             return Vector(output)
         
-        elif type(var) == float or int:
+        elif isinstance(var, (int, float)):
             for i in self.nums:
                 output.append(i+var)
             return Vector(output)
@@ -24,7 +23,6 @@ class Vector:
     def __sub__(self, var):
         output = []
         if isinstance(var, Vector):
-            output = []
             length = self.len if self.len >= var.len else var.len
             for i in range(length):
                 value1 = self.nums[i] if i < self.len else 0
@@ -32,10 +30,25 @@ class Vector:
                 output.append(value1-value2)
             return Vector(output)
         
-        elif type(var) == float or int:
+        elif isinstance(var, (int, float)):
             for i in self.nums:
                 output.append(i-var)
             return Vector(output)
+        
+    def __rsub__(self, var):
+     output = []
+     if isinstance(var, Vector):
+        length = self.len if self.len >= var.len else var.len
+        for i in range(length):
+            value1 = self.nums[i] if i < self.len else 0
+            value2 = var.nums[i] if i < var.len else 0
+            output.append(value2-value1)
+        return Vector(output)
+     
+     elif isinstance(var, (int, float)): 
+        for i in self.nums:
+            output.append(var-i)
+        return Vector(output)
 
 #Scaling and Division with itself and numbers
     def __mul__(self, var):
@@ -48,7 +61,7 @@ class Vector:
                 output.append(value1*value2)
             return Vector(output)
         
-        elif type(var) == float or int:
+        elif isinstance(var, (int, float)):
             for i in self.nums:
                 output.append(i*var)
             return Vector(output)
@@ -56,7 +69,6 @@ class Vector:
     def __truediv__(self, var):
         output = []
         if isinstance(var, Vector):
-            output = []
             length = self.len if self.len >= var.len else var.len
             for i in range(length):
                 value1 = self.nums[i] if i < self.len else 1
@@ -64,28 +76,41 @@ class Vector:
                 output.append(value1/value2)
             return Vector(output)
 
-        elif type(var) == float or int:
+        elif isinstance(var, (int, float)):
             for i in self.nums:
                 output.append(i/var)
             return Vector(output)
+        
+    def __rtruediv__(self, var):
+        output = []
+        if isinstance(var, Vector):
+            length = self.len if self.len >= var.len else var.len
+            for i in range(length):
+                value1 = self.nums[i] if i < self.len else 1
+                value2 = var.nums[i] if i < var.len else 1
+                output.append(value2/value1)
+            return Vector(output)
+
+        elif isinstance(var, (int, float)):
+            for i in self.nums:
+                output.append(var/i)
+            return Vector(output)
+        
+    def __round__(self, digits):
+        for i in range(self.len):
+            self.nums[i] = round(self.nums[i],digits)
+        return self
             
     def __neg__(self):
-        output = []
-        for i in self.nums:
-            output.append(i*-1)
-        return Vector(output)
+        for i in range(self.len):
+            self.nums[i] = -self.nums[i]
+        return self
     
     def __rmul__(self, var):
         return self.__mul__(var)
     
-    def __rtruediv__(self, var):
-        return self.__truediv__(var)
-    
     def __radd__(self, var):
         return self.__add__(var)
-    
-    def __rsub__(self, var):
-        return self.__sub__(var)
     
 #Shows as list
     def __repr__(self):
